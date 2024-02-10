80d1d57926ec6b354daeda8552846a18ac230503
Ana Núñez <123468393+ananb7@users.noreply.github.com>
2024-02-10 11:56:06 +0100

    Delete EDT_AD3_Eventos directory

--- EDT_AD3_Eventos/src/main/java/eventos/modelo/dao/PerfilDao.java
+++
@@ -1,9 +0,0 @@
-package eventos.modelo.dao;
-
-import eventos.modelo.entitis.Perfil;
-
-public interface PerfilDao {
-
-	Perfil findById(int idPeril);
-
-}
--- EDT_AD3_Eventos/src/main/java/eventos/modelo/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package eventos.modelo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eventos.modelo.entitis.Reserva;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Integer>{
-
-}
